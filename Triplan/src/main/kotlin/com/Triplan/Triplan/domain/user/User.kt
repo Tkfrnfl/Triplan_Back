386bf10b9a8a7d9f2a274e3565e167f4cabe537c
Eungyu-code <Agyu625@gmail.com>
2023-05-01 21:50:11 +0900

    feat: 기본 회원 엔티티 추가

---
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/domain/user/User.kt
@@ -0,0 +1,39 @@
+package com.Triplan.Triplan.domain.user
+
+import com.Triplan.Triplan.domain.BaseTimeEntity
+import jakarta.persistence.*
+
+@Entity
+class User(socialId: String, email: String, img: String, nickname: String, role: Role): BaseTimeEntity() {
+
+    @Id
+    @GeneratedValue
+    var id: Long = 0L;
+
+    var socialId: String? = null;
+
+    var email: String? = null;
+
+    var img: String? = null;
+
+    var nickname: String? = null;
+
+    @Enumerated(EnumType.STRING)
+    var role: Role? = null;
+
+    @Enumerated(EnumType.STRING)
+    var userState: UserState? = null;
+
+    var refreshToken: String? = null;
+
+    companion object {
+        val TAG = "User"
+        fun createKakaoUser(kakaoId: String, email: String, img: String, nickname: String): User {
+            return User(kakaoId, email, img, nickname, Role.KAKAO);
+        }
+
+        fun createNaverUser(naverId: String, email: String, img: String, nickname: String): User {
+            return User(naverId, email, img, nickname, Role.NAVER);
+        }
+    }
+}
