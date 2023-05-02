7d52598bee528d50e915177347f7dc7c5d0f94b1
Eungyu-code <Agyu625@gmail.com>
2023-05-02 22:34:10 +0900

    feat: json 관련 설정 변경

--- Triplan/build.gradle.kts
+++ Triplan/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 
 	//jwt
 	implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+	implementation("com.googlecode.json-simple:json-simple:1.1.1")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
 
