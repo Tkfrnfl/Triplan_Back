5d407a91693cea9a1e82b5b8e0cb6fdbc2a7abdc
Eungyu-code <Agyu625@gmail.com>
2023-05-05 16:55:04 +0900

    feat: 회원관리 로직이 수행되는 controller 추가

--- Triplan/src/main/kotlin/com/Triplan/Triplan/controller/UserController.kt
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/controller/UserController.kt
@@ -3,13 +3,11 @@ package com.Triplan.Triplan.controller
 import com.Triplan.Triplan.domain.user.dto.login.KakaoLoginRequestDto
 import com.Triplan.Triplan.domain.user.dto.login.LoginResponseDto
 import com.Triplan.Triplan.domain.user.dto.member.UserInfoDto
-import com.Triplan.Triplan.jwt.UserPrincipal
 import com.Triplan.Triplan.service.UserService
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
