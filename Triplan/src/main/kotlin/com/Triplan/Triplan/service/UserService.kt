2134cf415804d6dd7425cfcdfd367432da983571
Eungyu-code <Agyu625@gmail.com>
2023-05-16 22:09:32 +0900

    feat: login, userInfo, findSocialId 메소드 추가

--- Triplan/src/main/kotlin/com/Triplan/Triplan/service/UserService.kt
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/service/UserService.kt
@@ -1,47 +1,33 @@
 package com.Triplan.Triplan.service
 
 import com.Triplan.Triplan.domain.user.Role
-import com.Triplan.Triplan.domain.user.User
-import com.Triplan.Triplan.domain.user.dto.login.LoginResponseDto
-import com.Triplan.Triplan.jwt.JwtTokenProvider
+import com.Triplan.Triplan.domain.user.dto.member.UserInfoDto
 import com.Triplan.Triplan.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDateTime
 
 @Transactional
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val authService: AuthService,
-    private val jwtTokenProvider: JwtTokenProvider
+    private val authService: AuthService
 ) {
 
-    fun kakaoSignup(code: String): LoginResponseDto {
-        val accessToken = authService.getKakaoAccessTokenByCode(code)
-        val kakaoUser = authService.saveUserInfoByKakaoToken(accessToken)
-        val existUser = userRepository.findByRoleAndSocialId(Role.KAKAO, kakaoUser.get().socialId.toString())
+    fun login(accessToken: String) {
+        val user = authService.saveUserInfoByKakaoToken(accessToken).get()
 
-        return if (existUser.isPresent) {
-            login(existUser.get())
-        } else {
-            userRepository.save(kakaoUser.get())
-            login(kakaoUser.get())
+        if (!userRepository.findByRoleAndSocialId(Role.KAKAO, user.socialId).isPresent) {
+            userRepository.save(user)
         }
     }
 
-    fun login(user: User): LoginResponseDto {
-        val refreshToken = jwtTokenProvider.createRefreshToken(user.id)
-        authService.updateRefreshToken(user.id, refreshToken)
-        user.modifiedDate = LocalDateTime.now()
+    fun userInfo(userId: String?): UserInfoDto {
 
-        return LoginResponseDto.from(
-            jwtTokenProvider.createAccessToken(user.id),
-            refreshToken
-        )
+        return UserInfoDto(userRepository.findByRoleAndSocialId(Role.KAKAO, userId).get())
     }
 
-    fun findById(userId: Long): User {
-        return userRepository.findById(userId).get()
+    fun findSocialId(accessToken: String): String? {
+
+        return authService.saveUserInfoByKakaoToken(accessToken).get().socialId
     }
 }
