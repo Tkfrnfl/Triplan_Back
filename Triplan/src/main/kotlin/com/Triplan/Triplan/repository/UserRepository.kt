ab7c0040bf2cae5074058b552030b8f28dfb4f89
Eungyu-code <Agyu625@gmail.com>
2023-05-16 22:09:01 +0900

    remove: 회원의 refreshToken을 조회하는 메소드 삭제

--- Triplan/src/main/kotlin/com/Triplan/Triplan/repository/UserRepository.kt
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/repository/UserRepository.kt
@@ -3,14 +3,9 @@ package com.Triplan.Triplan.repository
 import com.Triplan.Triplan.domain.user.Role
 import com.Triplan.Triplan.domain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import java.util.Optional
 
 interface UserRepository: JpaRepository<User, Long> {
 
-    fun findByRoleAndSocialId(role: Role, socialId: String): Optional<User>
-
-    @Query(value = "SELECT u.refreshToken FROM Users u WHERE u.id=:id", nativeQuery = true)
-    fun findRefreshTokenById(@Param("id") userId: Long): String
+    fun findByRoleAndSocialId(role: Role, socialId: String?): Optional<User>
 }
