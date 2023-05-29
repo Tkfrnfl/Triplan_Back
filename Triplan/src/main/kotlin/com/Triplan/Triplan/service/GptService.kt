6db239e5ea8769074a720fbd4ecfd04499f642f7
Tkfrnfl <rul64@naver.com>
2023-05-29 21:45:09 +0900

    edit gpt location response

--- Triplan/src/main/kotlin/com/Triplan/Triplan/service/GptService.kt
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/service/GptService.kt
@@ -38,7 +38,7 @@ class GptService (@Value("\${auth.gpt.key}")
 
             val jsonObjectMessage=JSONObject()
             jsonObjectMessage.put("role","user")
-            jsonObjectMessage.put("content",question+" 장소를 기준으로")
+            jsonObjectMessage.put("content",question+" 장소만 나열")
 
             val messages=JSONArray()
             messages.add(jsonObjectMessage)
@@ -65,7 +65,7 @@ class GptService (@Value("\${auth.gpt.key}")
 
             val choices=(elem["choices"] as List<JSONObject>)
             val message=((choices[0])["message"] as JSONObject)["content"]
-            println(message.toString())
+           // println(message.toString())
             var parseByDay:List<String>
             if(message.toString().contains("Day")){
                 parseByDay= message.toString().split("Day")
@@ -77,19 +77,35 @@ class GptService (@Value("\${auth.gpt.key}")
             var tmpParse:List<String>
             var nounList:List<String>
 
-            for(i in 0 until parseByDay.count()){       //일차 별로 파싱
-                tmpParse= parseByDay[i].split("-")
+            for(i in 0 until parseByDay.count()){
+              tmpParse= parseByDay[i].split("-")
                 for(j in 0 until tmpParse.count()){
+                    var morning:String="오전"
+                    var launch:String="점심"
+                    var afternoon:String="오후"
+                    tmpParse[j].replace(morning,"")
+                    tmpParse[j].replace(launch,"")
+                    tmpParse[j].replace(afternoon,"")
                     parseByN.add(tmpParse[j])
                 }
-                nounList= analysisService.analysisSentence(parseByN)    // 형태소 분석기 실행
-                //받아온 명사들 정보처리 Service로 넘겨서 구글맵 검색기록으로 받아옴,
-                //장소로 나오는 경우 따로 일자별로 정보 저장
-                //println(nounList)
-                parseByN= arrayListOf()
             }
+            println(parseByN)
+
+
+//            for(i in 0 until parseByDay.count()){       //일차 별로 파싱
+//                tmpParse= parseByDay[i].split("-")
+//                for(j in 0 until tmpParse.count()){
+//                    parseByN.add(tmpParse[j])
+//                }
+//                nounList= analysisService.analysisSentence(parseByN)    // 형태소 분석기 실행
+//                //받아온 명사들 정보처리 Service로 넘겨서 구글맵 검색기록으로 받아옴,
+//                //장소로 나오는 경우 따로 일자별로 정보 저장
+//                //println(nounList)
+//                parseByN= arrayListOf()
+//            }
             // 일자별로 저장한 것을 이용하여 gpt 구문에 정보 표시
             //*  만약 성능이 너무 떨어질시 질문에"장소 별로" 추가 고려
+
             return
         }catch (exception: Exception) {
             println(exception.toString())
