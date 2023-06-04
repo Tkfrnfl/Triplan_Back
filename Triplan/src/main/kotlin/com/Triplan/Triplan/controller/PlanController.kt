95d5fac022be5e12e0eb026eb2547cc21be210a4
leesh <qwqw8019@naver.com>
2023-06-05 03:37:59 +0900

    feat: 최소시간 로직 반환 기능 컨트롤러 구현

--- Triplan/src/main/kotlin/com/Triplan/Triplan/controller/PlanController.kt
+++ Triplan/src/main/kotlin/com/Triplan/Triplan/controller/PlanController.kt
@@ -1,29 +1,23 @@
 package com.Triplan.Triplan.controller
 
+import com.Triplan.Triplan.domain.plan.Plan
 import com.Triplan.Triplan.domain.plan.dto.request.DayPlanRequestDto
 import com.Triplan.Triplan.domain.plan.dto.request.PlanRequestDto
+import com.Triplan.Triplan.service.PlanService
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class PlanController {
+class PlanController (val planService: PlanService){
 
     @MutationMapping
     fun requestPlanInformation(
         @RequestBody @Argument request: PlanRequestDto,
         @RequestBody @Argument requests: List<DayPlanRequestDto>
-    ): String {
-
-        println("startDate : " + request.startDate)
-        for (dayPlanRequestDto in requests) {
-            for (tripPlace in dayPlanRequestDto.tripPlaces) {
-                println(tripPlace)
-            }
-        }
-
-
-        return "success"
+    ): Plan {
+        return planService.route(request, requests)
     }
 }
